1daa8a86d56c306af41251102a5478753d28533e
Andrew Parks <parksdrew01@gmail.com>
2017-07-21 10:01:51 -0700

    covered all of the specs,and need to vertically output the numbers

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 //var firstNumbers = [1];
 //var secondNumbers = [1,2];
 //var thirdNumber = [1,2,"ping"];
-var firstNumber = 1;
-var secondNumber = [2];
-var thirdNumber = [1,2,"ping"];
-
-
+var firstNumber = ["1"];
+var secondNumber = ["2"];
+var thirdNumber = ["1", "2", "ping"];
+var fourthNumber = ["4","pong"];
+var fifthNumber = ["ping","7","8","ping","pong","11","ping","13","14","ping-pong"];
 
 
 
@@ -20,11 +20,44 @@ event.preventDefault();
 var userInput = $("#userInput").val();
 
 
-if(userInput === firstNumber) {
+if(userInput === firstNumber[0]) {
 
- $("#output h1").append(firstNumber);
+ $("#output h2").append(firstNumber[0]);
 
 }
+
+if(userInput === secondNumber[0]) {
+
+  var connect = firstNumber.concat(secondNumber);
+
+ $("#output h2").append(connect);
+
+}
+
+var addPing = thirdNumber.join();
+
+if(userInput === "3") {
+
+$("#output h2").append(addPing);
+
+}
+
+var secondPing = thirdNumber.concat(fourthNumber);
+
+if(userInput === "5") {
+
+$("#output h2").append(secondPing);
+
+}
+
+var firstPong = secondPing.concat(fifthNumber);
+
+if(userInput === "15") {
+
+$("#output h2").append(firstPong);
+
+}
+
 
 
   });
